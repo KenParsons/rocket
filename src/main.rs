b38d04588ff60a2785c6a98c481a412fc3d1e870
KenParsons <102046272+KenParsons@users.noreply.github.com>
2023-05-10 13:51:26 -0400

    working with basic cookies & query params

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use rocket::http::CookieJar;
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, from Rocket!"
+#[get("/?<name>")]
+fn index(name: Option<&str>, cookies: &CookieJar<'_>) -> String {
+    let intro: String = match name {
+        None => "Message: ".to_string(),
+        Some(name) => match name {
+            "" => "Message: ".to_string(),
+            name => format!("Message from {}: ", name),
+        },
+    };
+
+    match cookies.get("message") {
+        None => format!("No message!"),
+        Some(message_cookie) => format!("{}{}", intro, message_cookie.value()),
+    }
 }
 
 #[launch]
